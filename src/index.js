e90a4e1301d3e6c2e579108415fc2b6278004ba9
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-05-14 21:08:05 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/evidance-react">
+    <BrowserRouter basename="/final-project">
       <App />
     </BrowserRouter>
   </React.StrictMode>
