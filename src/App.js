9e13f5a5fc99a566eefabbfe1e6e3a59459811f9
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-05-21 20:38:57 +0300

    private routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import { Route, Routes } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./index.scss";
 
-import { Provider } from 'react-redux';
-import './index.scss';
+import { store } from "./redux/store";
 
-import { store } from './redux/store';
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { Bounce, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import MainPage from './pages/MainPage';
-import Login from './components/Login/Login';
+import Login from "./components/Login/Login";
+import MainPage from "./pages/MainPage";
 
 export const App = () => {
-	return (
-		<Provider store={store}>
-			<Routes>
-				<Route path='/' element={<MainPage />} />
-				<Route path='/login' element={<Login />} />
-			</Routes>
+  return (
+    <Provider store={store}>
+      <Routes>
+        {/* <Route path="/" element={<Navigate to="/login" replace />} />
 
-			<ToastContainer
-				position='top-right'
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme='light'
-				transition={Bounce}
-			/>
-		</Provider>
-	);
+        <Route path="/login" element={<Login />} /> */}
+        <Route path="/" element={<Navigate to="/start" replace />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/start"
+          element={
+            // <PrivateRoute>
+            <MainPage />
+            // </PrivateRoute>
+          }
+        />
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </Provider>
+  );
 };
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,60 +1,95 @@
-import './Login.scss';
-import logo from '../../img/logo.svg';
-import googleLogo from '../../img/google-logo.svg';
-import loginBackground from '../../img/login-background.svg';
-import loginBackgroundSecond from '../../img/login-background-second.svg';
+import googleLogo from "../../img/google-logo.svg";
+import loginBackgroundSecond from "../../img/login-background-second.svg";
+import loginBackground from "../../img/login-background.svg";
+import logo from "../../img/logo.svg";
+import "./Login.scss";
+
+// import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-	return (
-		<div className='login-container'>
-			<img src={logo} alt='' className='login-container-logo' />
-			<img
-				src={loginBackground}
-				alt=''
-				className='login-container-background'
-			/>
-			<img
-				src={loginBackgroundSecond}
-				alt=''
-				className='login-container-background-second'
-			/>
-			<div className='login-title-container'>
-				<h1>InvestIQ</h1>
-				<h3>Smart Finance</h3>
-			</div>
-			<div className='login-form-container'>
-				<form className='login-form'>
-					<div className='google-login'>
-						<span>Ви можете авторизуватися за допомогою акаунта Google</span>
-						<button>
-							<img src={googleLogo} alt='googleLogo' />
-							<span>Google</span>
-						</button>
-					</div>
+  //   const navigate = useNavigate();
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
 
-					<div className='email-login'>
-						<span>
-							Або увійти за допомогою ел. пошти та праолю після реєстрації
-						</span>
+  //   useEffect(() => {
+  //     const token = localStorage.getItem("token");
+  //     if (token) {
+  //       navigate("/start");
+  //     }
+  //   }, [navigate]);
 
-						<div className='email-login-email'>
-							<span>Електронна пошта:</span>
-							<input type='email' placeholder='your@email.com' />
-						</div>
-						<div className='email-login-password'>
-							<span>Пароль:</span>
-							<input type='password' placeholder='••••••••' />
-						</div>
-					</div>
+  //   const handleLogin = (e) => {
+  //     e.preventDefault();
 
-					<div className='form-submit-container'>
-						<button className='login-btn'>Увійти</button>
-						<button className='register-btn'>реєстрація</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+  //     if (email && password) {
+  //       localStorage.setItem("token", "mock-token");
+  //       navigate("/start");
+  //     } else {
+  //       console.log("Введіть email та пароль");
+  //     }
+  //   };
+
+  return (
+    <div className="login-container">
+      <img src={logo} alt="" className="login-container-logo" />
+      <img
+        src={loginBackground}
+        alt=""
+        className="login-container-background"
+      />
+      <img
+        src={loginBackgroundSecond}
+        alt=""
+        className="login-container-background-second"
+      />
+      <div className="login-title-container">
+        <h1>InvestIQ</h1>
+        <h3>Smart Finance</h3>
+      </div>
+      <div className="login-form-container">
+        <form className="login-form">
+          <div className="google-login">
+            <span>Ви можете авторизуватися за допомогою акаунта Google</span>
+            <button>
+              <img src={googleLogo} alt="googleLogo" />
+              <span>Google</span>
+            </button>
+          </div>
+
+          <div className="email-login">
+            <span>
+              Або увійти за допомогою ел. пошти та праолю після реєстрації
+            </span>
+
+            <div className="email-login-email">
+              <span>Електронна пошта:</span>
+              <input
+                type="email"
+                // placeholder="your@email.com"
+                // value={email}
+                // onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="email-login-password">
+              <span>Пароль:</span>
+              <input
+                type="password"
+                placeholder="••••••••"
+                // value={password}
+                // onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="form-submit-container">
+            <button className="login-btn">Увійти</button>
+            <button className="register-btn">реєстрація</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
