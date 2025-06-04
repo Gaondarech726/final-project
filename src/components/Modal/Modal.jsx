a3dcee631511b79e52fd41db3d10f0cb3787124a
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-06-04 20:33:38 +0300

    fix overflow

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,7 @@ export const Modal = ({ onConfirm, onCancel }) => {
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = "auto";
+      document.body.style.overflow = "";
     };
   }, []);
 
